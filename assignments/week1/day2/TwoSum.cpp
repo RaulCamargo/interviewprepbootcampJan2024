f982400eb67779ae844beab9068819c77114c2b8
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2024-01-04 04:38:40 -0500

    Changed IDE, +TwoSum.cpp

--- assignments/week1/day2/TwoSum.cpp
+++ assignments/week1/day2/TwoSum.cpp
@@ -1,2 +1,31 @@
 /*
-Problem*/
+    LeetCode 1: Two Sum
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> map;
+        vector<int> result;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if(map.find(complement) != map.end()) {
+                result.push_back(map[complement]);
+                result.push_back(i);
+                return result;
+            }
+            map[nums[i]] = i;
+        }
+
+        return result;
+    }
+};
